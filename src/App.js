8f0fbc2e27693759680c0d7d8aa84aca8343545f
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-09-16 20:23:42 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div>{/* <h2>yes</h2> */}</div>;
+  return (
+    <div>
+      <h2>hello world</h2>
+      {/* <h2>yes</h2> */}
+    </div>
+  );
 }
 
 export default App;
