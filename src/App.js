eee6fa3683c6d95bb187815d7d2f5d130b04e270
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-09-18 23:27:43 +0100

    function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Cart from './components/Cart/Cart';
+
 function App() {
   return (
     <div>
-      <h2>hello world</h2>
+      {/* <h2>hello world</h2> */}
       {/* <h2>yes</h2> */}
     </div>
   );
