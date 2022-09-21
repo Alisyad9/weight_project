d2526203e89ebcc79c3e857d43c9af174bb7374a
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-09-21 22:56:32 +0100

    .

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <React.Fragment>
       <header className={classes.header}>
-        <h1>Meals</h1> <button>Cart</button>
+        {/* <h1>Meals</h1> <button>Cart</button> */}
       </header>
       <div className={classes['main-image']}>
         <img src={cloud} alt="clouds in the sky" />
