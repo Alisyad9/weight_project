f392f6f971cc49a6dc8ffb7179b1d1066a0dfc78
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-10-08 19:49:46 +0100

    .

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,19 +1,19 @@
-// import React from 'react';
+import React from 'react';
 
-// import classes from './Header.module.css';
-// import cloud from '../../image/cloud.jpg';
+import classes from './Header.module.css';
+import cloud from '../../image/cloud.jpg';
 
-// const Header = () => {
-//   return (
-//     <React.Fragment>
-//       <header className={classes.header}>
-//         {/* <h1>Meals</h1> <button>Cart</button> */}
-//       </header>
-//       {/* <div className={classes['main-image']}>
-//         <img src={cloud} alt="clouds in the sky" />
-//       </div> */}
-//     </React.Fragment>
-//   );
-// };
+const Header = () => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>Meals</h1> <button>Cart</button>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={cloud} alt="clouds in the sky" />
+      </div>
+    </React.Fragment>
+  );
+};
 
-// export default Header;
+export default Header;
