f23397b334486f1ff73074545c339bb5b374598e
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-10-08 20:29:43 +0100

    .

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import classes from './Header.module.css';
 import cloud from '../../image/cloud.jpg';
+import HeaderButton from './HeaderButton';
 
 const Header = () => {
   return (
     <React.Fragment>
       <header className={classes.header}>
-        <h1>Meals</h1> <button>Cart</button>
+        <h1>Meals</h1> <HeaderButton />
       </header>
       <div className={classes['main-image']}>
         <img src={cloud} alt="clouds in the sky" />
---
+++ src/components/Layout/HeaderButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Cart from '../Cart/Cart';
+import classes from './HeaderButton.module.css';
+import logo from '../../image/logo.svg';
+
+const HeaderButton = () => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        {' '}
+        <Cart />
+      </span>
+
+      <span className={classes.badge}>3</span>
+    </button>
+  );
+};
+
+export default HeaderButton;
