2cdbda87ec6560e89ebab89660c92cb95771689a
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-10-22 23:23:55 +0100

    .

--- src/components/Layout/HeaderButton.js
+++ src/components/Layout/HeaderButton.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import Cart from '../Cart/Cart';
-import classes from './HeaderButton.module.css';
-import logo from '../../image/logo.svg';
+// import React from 'react';
+// import Cart from '../Cart/Cart';
+// import classes from './HeaderButton.module.css';
+// import logo from '../../image/logo.svg';
 
-const HeaderButton = () => {
-  return (
-    <button className={classes.button}>
-      <span className={classes.icon}>
-        <Cart />
-      </span>
+// const HeaderButton = () => {
+//   return (
+//     <button className={classes.button}>
+//       <span className={classes.icon}>
+//         <Cart />
+//       </span>
 
-      <span className={classes.badge}>3</span>
-    </button>
-  );
-};
+//       <span className={classes.badge}>3</span>
+//     </button>
+//   );
+// };
 
-export default HeaderButton;
+// export default HeaderButton;
