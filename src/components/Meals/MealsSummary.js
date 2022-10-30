5db074f350093d5e7e4270a80fd36643afe62f4e
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-10-30 13:42:00 +0000

    summary function

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MealsSummary = () => {
+  return (
+    <section>
+      <h2>Food, delivered to you</h2>
+      <p>choose what food you want to eat from a broad selection of dishes.</p>
+      <p>
+        All dishes are cooked with high quality ingredients, which will
+        tantalise your appetite
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
