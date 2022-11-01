d5292d1d13e91fdf3046cbe729a831c70f248e4f
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-11-01 21:19:34 +0000

    meal summary

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './MealsSummary.module.css';
 
 const MealsSummary = () => {
   return (
-    <section>
+    <section className={classes.summary}>
       <h2>Food, delivered to you</h2>
       <p>choose what food you want to eat from a broad selection of dishes.</p>
       <p>
