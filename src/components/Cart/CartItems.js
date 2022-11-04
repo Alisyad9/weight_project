921a1bae000c92367eff57ae96c6c1784a3832f6
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-11-04 23:50:58 +0000

    items

---
+++ src/components/Cart/CartItems.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Modal from '../UI/Modal';
+
+import classes from './CartItems.module.css';
+
+const CartItems = (props) => {
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: 'c1', name: 'Burger', amount: 2, price: 12.22 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>total amount:</span>
+        <span>35</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}> Close</button>
+        <button classes={classes.button}>order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default CartItems;
