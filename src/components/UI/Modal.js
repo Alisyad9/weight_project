bd51ac364662213372cf7aec1157e71c0c28a481
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-11-04 23:51:56 +0000

    .

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react';
+import classes from './Modal.module.css';
+import ReactDOM from 'react-dom';
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content} />
+      {props.children}
+    </div>
+  );
+};
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+  return (
+    // <Fragment>
+    //   <Backdrop />
+    //   <ModalOverlay>{props.children}</ModalOverlay>
+    // </Fragment>
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
