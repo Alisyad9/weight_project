1d7c487a0744b29cbcf3efdb8f8b401129f20f9d
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-11-10 17:44:05 +0000

    modal closes onclick

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import classes from './Modal.module.css';
 import ReactDOM from 'react-dom';
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClosed} />;
 };
 
 const ModalOverlay = (props) => {
@@ -18,13 +18,17 @@ const ModalOverlay = (props) => {
 const portalElement = document.getElementById('overlays');
 
 const Modal = (props) => {
+  console.log('inside the modal props: ', props);
   return (
     // <Fragment>
     //   <Backdrop />
     //   <ModalOverlay>{props.children}</ModalOverlay>
     // </Fragment>
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <Backdrop onClosed={props.onClosed} />,
+        portalElement
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
