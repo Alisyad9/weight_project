85154d95b055fe71c820df9f6f719a365d05ecbb
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-11-19 23:03:32 +0000

    submit handler function

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,8 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
   const amountInputRef = useRef();
 
   const submitHandler = (event) => {
@@ -10,23 +11,37 @@ const MealItemForm = (props) => {
 
     const enteredAmount = amountInputRef.current.value;
     const enteredAmountNumber = +enteredAmount;
+    console.log(
+      'inside the if statement in MealItemForm',
+      enteredAmount.trim().length
+    );
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmount < 1 ||
+      enteredAmount > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
   };
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
-          ref: { amountInputRef },
           //   id: 'amount',
           id: 'amount_' + props.id,
           type: 'number',
           min: '1',
-          max: '10',
+          max: '5',
           step: '1',
           defaultValue: '1',
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>please enter a valid amount between 1 and 5</p>}
     </form>
   );
 };
